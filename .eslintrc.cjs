3fb89c77c46294cb99279b048c09c219e22cd8b6
Muhammad Naufal Arkan Rafii <muhnaufal251@gmail.com>
2022-11-07 21:41:41 +0700

    Initial Setup

---
+++ .eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:vue/vue3-recommended", "prettier"],
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+  },
+  parserOptions: {
+    sourceType: "module",
+  },
+};
