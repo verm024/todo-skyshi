370c800a75df8b258184bdf84766dcf6177590ca
Muhammad Naufal Arkan Rafii <muhnaufal251@gmail.com>
2022-11-08 10:50:05 +0700

    Add Other Routes

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,15 @@ const routes = [
     name: "Home",
     component: () => import("@/views/HomeView.vue"),
   },
+  {
+    path: "/detail/:id",
+    name: "Detail",
+    component: () => import("@/views/DetailView.vue"),
+  },
+  {
+    path: "*",
+    name: "Unavailable",
+  },
 ];
 
 const router = createRouter({
