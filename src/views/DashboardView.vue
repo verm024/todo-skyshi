0079ddbe7eac8f472875ece7b81133b92a646ae2
Muhammad Naufal Arkan Rafii <muhnaufal251@gmail.com>
2022-11-09 10:13:38 +0700

    Implement Get Activity List

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -1,7 +1,18 @@
+<template>
+  <div class="d-flex justify-content-between align-items-center mb-4">
+    <base-text _as="h4" :font-weight="700">Activity</base-text>
+    <base-button icon="bi bi-plus-lg">Tambah</base-button>
+  </div>
+  <activity-list :activities="activities" />
+</template>
+
 <script>
 import { BaseText } from "@/components/atom";
 import { BaseButton } from "@/components/molecules";
 import { ActivityList } from "@/components/organism";
+import api from "@/utils/api";
+
+import { ref, onMounted } from "vue";
 
 export default {
   name: "DashboardView",
@@ -10,67 +21,26 @@ export default {
     BaseButton,
     ActivityList,
   },
+  setup() {
+    const activities = ref([]);
+    const fetchActivities = async () => {
+      try {
+        const res = await api().get(
+          "/activity-groups?email=muhnaufal251%40gmail.com"
+        );
+        const { data } = res.data;
+        activities.value = data;
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    onMounted(() => fetchActivities());
+
+    return {
+      activities,
+    };
+  },
 };
 </script>
-
-<template>
-  <div class="d-flex justify-content-between align-items-center mb-4">
-    <base-text _as="h4" :font-weight="700">Activity</base-text>
-    <base-button icon="bi bi-plus-lg">Tambah</base-button>
-  </div>
-  <activity-list
-    :activities="[
-      {
-        created_at: '2022-11-08T12:34:25.240Z',
-        updated_at: '2022-11-08T12:34:25.240Z',
-        id: 13617,
-        title: 'coba 1',
-        email: 'test@gmail.com',
-      },
-      {
-        created_at: '2022-11-08T12:34:25.240Z',
-        updated_at: '2022-11-08T12:34:25.240Z',
-        id: 13618,
-        title: 'coba 2',
-        email: 'test@gmail.com',
-      },
-      {
-        created_at: '2022-11-08T12:34:25.240Z',
-        updated_at: '2022-11-08T12:34:25.240Z',
-        id: 13619,
-        title: 'coba 3',
-        email: 'test@gmail.com',
-      },
-      {
-        created_at: '2022-11-08T12:34:25.240Z',
-        updated_at: '2022-11-08T12:34:25.240Z',
-        id: 13620,
-        title: 'coba 4',
-        email: 'test@gmail.com',
-      },
-      {
-        created_at: '2022-11-08T12:34:25.240Z',
-        updated_at: '2022-11-08T12:34:25.240Z',
-        id: 13621,
-        title: 'coba 4',
-        email: 'test@gmail.com',
-      },
-      {
-        created_at: '2022-11-08T12:34:25.240Z',
-        updated_at: '2022-11-08T12:34:25.240Z',
-        id: 13622,
-        title: 'coba 4',
-        email: 'test@gmail.com',
-      },
-      {
-        created_at: '2022-11-08T12:34:25.240Z',
-        updated_at: '2022-11-08T12:34:25.240Z',
-        id: 13623,
-        title: 'coba 4',
-        email: 'test@gmail.com',
-      },
-    ]"
-  />
-</template>
 
 <style scoped></style>
