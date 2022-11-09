066f09df81e650ddc7e35a2073925a50eaa4a370
Muhammad Naufal Arkan Rafii <muhnaufal251@gmail.com>
2022-11-09 16:57:10 +0700

    Implement Activity Title

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="d-flex justify-content-between align-items-center mb-4">
-    <base-text _as="h4" :font-weight="700">Activity</base-text>
+    <div class="d-flex align-items-center">
+      <div role="button" class="me-4">
+        <router-link to="/">
+          <base-text _as="h5"><i class="bi bi-chevron-left"></i></base-text
+        ></router-link>
+      </div>
+      <base-text _as="h4" :font-weight="700">{{ activityTitle }}</base-text>
+      <div role="button" class="ms-4">
+        <base-text _as="h6" color="#A4A4A4"
+          ><i class="bi bi-pencil"></i
+        ></base-text>
+      </div>
+    </div>
     <base-button icon="bi bi-plus-lg">Tambah</base-button>
   </div>
   <todo-list :todos="todos" />
@@ -25,13 +37,13 @@ export default {
   setup() {
     const route = useRoute();
     const todos = ref([]);
+    const activityTitle = ref("");
     const fetchTodos = async () => {
       try {
-        const res = await api().get(
-          `/todo-items?activity_group_id=${route.params.id}`
-        );
-        const { data } = res.data;
-        todos.value = data;
+        const res = await api().get(`/activity-groups/${route.params.id}`);
+        const { data } = res;
+        todos.value = data?.todo_items;
+        activityTitle.value = data?.title;
       } catch (error) {
         console.error(error);
       }
@@ -40,6 +52,7 @@ export default {
 
     return {
       todos,
+      activityTitle,
     };
   },
 };
