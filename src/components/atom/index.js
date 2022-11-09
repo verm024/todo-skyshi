e540d096f26c884bccb9354ab58e9fa5a424bad4
Muhammad Naufal <45099597+verm024@users.noreply.github.com>
2022-11-09 19:10:07 +0700

    Task / 12 / Create LabeledInput Molecule (#13)

--- src/components/atom/index.js
+++ src/components/atom/index.js
@@ -1,3 +1,4 @@
 export { default as BaseText } from "@/components/atom/BaseText";
 export { default as ColoredCircle } from "@/components/atom/ColoredCircle";
 export { default as BaseCheckbox } from "@/components/atom/BaseCheckbox";
+export { default as BaseInput } from "@/components/atom/BaseInput";
