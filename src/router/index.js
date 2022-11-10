fc9f0fa1614a923b8ec0694b996954c6e6bcc61a
Muhammad Naufal <45099597+verm024@users.noreply.github.com>
2022-11-10 13:52:12 +0700

    Task / 20 / Protect Routes (#19)
    * Implement Route Protection for Unavailable and Detail Page
    
    * Move Unavailable Route Protection to BeforeEnter
    
    * Remove Conditional of Unavailable Route BeforeEnter

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createWebHistory, createRouter } from "vue-router";
 
+import api from "@/utils/api";
+
 const routes = [
   {
     path: "/",
@@ -10,10 +12,26 @@ const routes = [
     path: "/detail/:id",
     name: "Detail",
     component: () => import("@/views/DetailView.vue"),
+    beforeEnter: async (to, from, next) => {
+      try {
+        const res = await api().get(`/activity-groups/${to.params.id}`);
+        if (res.status === 200) {
+          next();
+        } else {
+          next("/");
+        }
+      } catch (error) {
+        next("/");
+        console.error(error);
+      }
+    },
   },
   {
     path: "/:pathMatch(.*)*",
     name: "Unavailable",
+    beforeEnter: (to, from, next) => {
+      next("/");
+    },
   },
 ];
 
