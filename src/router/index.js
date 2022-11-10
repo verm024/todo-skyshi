652ea92b3818db6823ebc64c5b4ba2da64512e07
Muhammad Naufal Arkan Rafii <muhnaufal251@gmail.com>
2022-11-10 13:40:36 +0700

    Implement Route Protection for Unavailable and Detail Page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
 import { createWebHistory, createRouter } from "vue-router";
 
+import api from "@/utils/api";
+
 const routes = [
   {
     path: "/",
@@ -10,6 +12,19 @@ const routes = [
     path: "/detail/:id",
     name: "Detail",
     component: () => import("@/views/DetailView.vue"),
+    beforeEnter: async (to, from, next) => {
+      try {
+        const res = await api().get(`/activity-groups/${to.params.id}`);
+        if (res.status === 200) {
+          next();
+        } else {
+          next("/");
+        }
+      } catch (error) {
+        next("/");
+        console.error(error);
+      }
+    },
   },
   {
     path: "/:pathMatch(.*)*",
@@ -20,6 +35,14 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.name == "Unavailable") {
+    next("/");
+  } else {
+    next();
+  }
 });
 
 export default router;
